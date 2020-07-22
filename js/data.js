f7b45f8fbef286088bc675494eb85b6a45fb999f
zyying94264 <827237844@qq.com>
2020-07-22 15:31:18 +0800

    云盘整体内容说明文档,增加少量数据

--- js/data.js
+++ js/data.js
@@ -1,57 +1,72 @@
 var data = [
-	{
-		id:0,
-		pid:-1,
-		title:"微云"
-	},
-	{
-		id:1,
-		pid:0,
-		title:"我的文档"
-	},
-	{
-		id:2,
-		pid:0,
-		title:"我的音乐"
-	},
-	{
-		id:2999,
-		pid:1,
-		title:"js程序设计"
-	},
-	{
-		id:29000,
-		pid:1,
-		title:"js权威指南"
-	},
-	{
-		id:23333,
-		pid:2,
-		title:"王杰"
-	},
-	{
-		id:244444,
-		pid:2,
-		title:"张国荣"
-	},
-	{
-		id:3,
-		pid:2,
-		title:"周杰伦"
-	},
-	{
-		id:4000,
-		pid:3,
-		title:"稻香"
-	},
-	{
-		id:4,
-		pid:3,
-		title:"发如雪"
-	},
-	{
-		id:600,
-		pid:3,
-		title:"夜曲"
-	}
+  {
+    id: 0,
+    pid: -1,
+    title: "云文件"
+  },
+  {
+    id: 1,
+    pid: 0,
+    title: "我的文档"
+  },
+  {
+    id: 2,
+    pid: 0,
+    title: "我的音乐"
+  },
+  {
+    id: 3,
+    pid: 0,
+    title: "我的图片"
+  },
+  {
+    id: 2999,
+    pid: 1,
+    title: "js程序设计"
+  },
+  {
+    id: 29000,
+    pid: 1,
+    title: "js权威指南"
+  },
+  {
+    id: 23333,
+    pid: 2,
+    title: "王杰"
+  },
+  {
+    id: 244444,
+    pid: 2,
+    title: "张国荣"
+  },
+  {
+    id: 230,
+    pid: 2,
+    title: "周杰伦"
+  },
+  {
+    id: 4000,
+    pid: 230,
+    title: "稻香"
+  },
+  {
+    id: 4,
+    pid: 230,
+    title: "发如雪"
+  },
+  {
+    id: 600,
+    pid: 230,
+    title: "夜曲"
+  },
+  {
+    id: 3290,
+    pid: 3,
+    title: "小猫"
+  },
+  {
+    id: 3287,
+    pid: 3,
+    title: "小狗"
+  }
 ]
